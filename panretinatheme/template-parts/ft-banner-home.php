e1ef76677c0f224e4061236f257e50911360e876
Brandão <lucas.brandao@planetaw.ag>
2025-08-13 16:06:48 -0300

    Subindo ajustes

--- panretinatheme/template-parts/ft-banner-home.php
+++ panretinatheme/template-parts/ft-banner-home.php
@@ -1,4 +1,4 @@
-<section class="relative pb-50 py-20 before:absolute before:top-0 before:left-0 before:size-full before:bg-[url(../../../../uploads/banner-home.png)] before:bg-no-repeat before:bg-cover before:bg-left 2xl:before:bg-right">
+<section class="relative pb-50 py-20 before:absolute before:top-0 before:left-0 before:size-full before:bg-[url(../../../../uploads/banner-mobile-550x400-1.png)] lg:before:bg-[url(../../../../uploads/banner-home.png)] 2xl:before:bg-[url(../../../../uploads/banner-home02.png)] before:bg-no-repeat before:bg-cover 3xl:before:bg-size-[80%] before:bg-left 2xl:before:bg-right">
     <div class="container relative">
         <img src="<?= get_option('options_logo_header'); ?>" class="w-76 object-contain max-md:mx-auto">
     </div>
